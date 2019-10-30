df30a44e3e6b05b693afd8f490a1e13d7486fc41
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-10-30 08:20:40 +0700

    commit

--- php/db.php
+++ php/db.php
@@ -1,9 +1,3 @@
 <?php
 $c = new mysqli("localhost", "iptc8859_admin", "HelloWorld@123");
 $c->select_db("iptc8859_iptv");
-echo "Connect error: " . $c->connect_error . "\n";
-if ($c) {
-	echo "Connection succeed\n";
-} else {
-	echo "Connection error\n";
-}
