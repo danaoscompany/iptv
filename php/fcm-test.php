d813486021b7f180286a1d4bd5a3ff83829d4f34
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-10-31 14:13:50 +0700

    commit

--- php/fcm-test.php
+++ php/fcm-test.php
@@ -6,7 +6,10 @@ $url = 'https://fcm.googleapis.com/fcm/send';
 $server_key = 'AAAAQ0HaK2k:APA91bFS-FA9qbzC7AQ25VJS9W_9vaO5yLPd9XcbaNwaFCvpROH8J1Tu8QEDNKdS8joA49QSg0v6YsaZR2-fcE_yHm4smeqmIAPXQXdRmhzue4zg-768pNbDdj3X4Ewwz67yGKvkCua0';
 
 $notification = array('title' =>"This is title" , 'body' => "This is text", 'sound' => 'default', 'badge' => '1');
-$data = array('to' => $receiverRegistrationToken, 'notification' => $notification,'priority'=>'high', 'data'=>'{"Nick" : "Mario", "Room" : "PortugalVSDenmark"}');
+$data = array('to' => $receiverRegistrationToken, 'notification' => $notification,'priority'=>'high', 'data'=>array(
+        "name"=>"dana",
+        "type"=>"new_message"
+        ));
 //header with content_type api key
 $headers = array(
 	'Content-Type:application/json',
