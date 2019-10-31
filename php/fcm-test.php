543c8f20c1d7020869dd08563ddb34e9e3eb3f7a
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-10-31 13:57:50 +0700

    commit

--- php/fcm-test.php
+++ php/fcm-test.php
@@ -1,17 +1,31 @@
 <?php
 $receiverRegistrationToken = $_POST["registration_token"];
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://fcm.googleapis.com/v1/projects/joss-player/messages:send");
-$header = array();
-$header[] = "Content-Type: application/json";
-$header[] = "Authorization: Bearer " . $accessToken;
-curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 60);
-curl_setopt($curl, CURLOPT_TIMEOUT, 60);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, '{"message":{"notification": {"title": "New message", "body": "This is message"},"token": "' . $receiverRegistrationToken . '", "data": {"type": "new_message"}}}');
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-curl_exec($curl);
-curl_close($curl);
+//FCM api URL
+$url = 'https://fcm.googleapis.com/fcm/send';
+//api_key available in Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key
+$server_key = 'AAAAQ0HaK2k:APA91bFS-FA9qbzC7AQ25VJS9W_9vaO5yLPd9XcbaNwaFCvpROH8J1Tu8QEDNKdS8joA49QSg0v6YsaZR2-fcE_yHm4smeqmIAPXQXdRmhzue4zg-768pNbDdj3X4Ewwz67yGKvkCua0';
+			
+$fields = array();
+$fields['data'] = $data;
+$fields['to'] = $receiverRegistrationToken;
+//header with content_type api key
+$headers = array(
+	'Content-Type:application/json',
+  'Authorization:key='.$server_key
+);
+			
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+$result = curl_exec($ch);
+if ($result === FALSE) {
+	die('FCM Send Error: ' . curl_error($ch));
+} else {
+	echo "Success\n";
+}
+curl_close($ch);
