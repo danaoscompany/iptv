bb462173c6ba2620130f3f0316d8e8f41d494e50
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-10-31 22:42:39 +0700

    commit

--- php/get-user-info-by-email.php
+++ php/get-user-info-by-email.php
@@ -1,6 +1,7 @@
 <?php
 include 'db.php';
-$results = $c->query("SELECT * FROM `users` WHERE email='danaoscompany@gmail.com'");
+$email = $_POST["email"];
+$results = $c->query("SELECT * FROM users WHERE email='" . $email . "'");
 if ($results && $results->num_rows > 0) {
     echo json_encode($results->fetch_assoc());
 } else {
