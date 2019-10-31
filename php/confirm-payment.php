0a2ce5f1c9eeb6e6fafb84099ca5c40dd277df01
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-10-31 13:16:09 +0700

    commit

--- php/confirm-payment.php
+++ php/confirm-payment.php
@@ -4,4 +4,26 @@ $data = file_get_contents("php://input");
 $obj = json_decode($data);
 $externalID = $obj["external_id"];
 $c->query("UPDATE payments SET status=1 WHERE external_id='" . $externalID . "'");
-
+//FCM API end-point
+$url = 'https://fcm.googleapis.com/fcm/send';
+//api_key in Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key
+$server_key = 'AAAAQ0HaK2k:APA91bFS-FA9qbzC7AQ25VJS9W_9vaO5yLPd9XcbaNwaFCvpROH8J1Tu8QEDNKdS8joA49QSg0v6YsaZR2-fcE_yHm4smeqmIAPXQXdRmhzue4zg-768pNbDdj3X4Ewwz67yGKvkCua0';
+//header with content_type api key
+$headers = array(
+    'Content-Type:application/json',
+    'Authorization:key='.$server_key
+);
+//CURL request to route notification to FCM connection server (provided by Google)
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+$result = curl_exec($ch);
+if ($result === FALSE) {
+    die('Oops! FCM Send Error: ' . curl_error($ch));
+}
+curl_close($ch);
---
+++ php/fcm-test.php
@@ -0,0 +1,17 @@
+<?php
+$receiverRegistrationToken = $_POST["registration_token"];
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://fcm.googleapis.com/v1/projects/joss-player/messages:send");
+$header = array();
+$header[] = "Content-Type: application/json";
+$header[] = "Authorization: Bearer " . $accessToken;
+curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 60);
+curl_setopt($curl, CURLOPT_TIMEOUT, 60);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, '{"message":{"notification": {"title": "New message", "body": "' . $body . '"},"token": "' . $receiverRegistrationToken . '", "data": {"type": "new_message"}}}');
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_exec($curl);
+curl_close($curl);
