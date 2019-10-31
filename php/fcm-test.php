41583050ef39c3e55d8c3b9b348f0b414c2c5873
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-10-31 13:54:30 +0700

    commit

--- php/fcm-test.php
+++ php/fcm-test.php
@@ -10,7 +10,7 @@ curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 60);
 curl_setopt($curl, CURLOPT_TIMEOUT, 60);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, '{"message":{"notification": {"title": "New message", "body": "' . $body . '"},"token": "' . $receiverRegistrationToken . '", "data": {"type": "new_message"}}}');
+curl_setopt($curl, CURLOPT_POSTFIELDS, '{"message":{"notification": {"title": "New message", "body": "This is message"},"token": "' . $receiverRegistrationToken . '", "data": {"type": "new_message"}}}');
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 curl_exec($curl);
