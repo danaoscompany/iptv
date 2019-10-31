01983a35fceae6f6b233b156cd777d76f4363940
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-10-31 14:09:21 +0700

    commit

--- php/fcm-test.php
+++ php/fcm-test.php
@@ -6,11 +6,7 @@ $url = 'https://fcm.googleapis.com/fcm/send';
 $server_key = 'AAAAQ0HaK2k:APA91bFS-FA9qbzC7AQ25VJS9W_9vaO5yLPd9XcbaNwaFCvpROH8J1Tu8QEDNKdS8joA49QSg0v6YsaZR2-fcE_yHm4smeqmIAPXQXdRmhzue4zg-768pNbDdj3X4Ewwz67yGKvkCua0';
 
 $notification = array('title' =>"This is title" , 'body' => "This is text", 'sound' => 'default', 'badge' => '1');
-$data = array('to' => $token, 'notification' => $notification,'priority'=>'high', 'data'=>'{\"type\": \"new_message\"}');
-			
-$fields = array();
-$fields['data'] = $data;
-$fields['to'] = $receiverRegistrationToken;
+$data = array('to' => $receiverRegistrationToken, 'notification' => $notification,'priority'=>'high', 'data'=>'{\"type\": \"new_message\"}');
 //header with content_type api key
 $headers = array(
 	'Content-Type:application/json',
@@ -24,7 +20,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 $result = curl_exec($ch);
 if ($result === FALSE) {
 	die('FCM Send Error: ' . curl_error($ch));
