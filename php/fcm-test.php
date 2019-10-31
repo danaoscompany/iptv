5e62c3df8d670b6dd9ea1c55f8ba9d9d843e1da9
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-10-31 14:06:07 +0700

    commit

--- php/fcm-test.php
+++ php/fcm-test.php
@@ -4,9 +4,12 @@ $receiverRegistrationToken = $_POST["registration_token"];
 $url = 'https://fcm.googleapis.com/fcm/send';
 //api_key available in Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key
 $server_key = 'AAAAQ0HaK2k:APA91bFS-FA9qbzC7AQ25VJS9W_9vaO5yLPd9XcbaNwaFCvpROH8J1Tu8QEDNKdS8joA49QSg0v6YsaZR2-fcE_yHm4smeqmIAPXQXdRmhzue4zg-768pNbDdj3X4Ewwz67yGKvkCua0';
+
+$notification = array('title' =>"This is title" , 'body' => "This is text", 'sound' => 'default', 'badge' => '1');
+$data = array('to' => $token, 'notification' => $notification,'priority'=>'high');
 			
 $fields = array();
-$fields['data'] = "{\"type\": \"new_message\"}";
+$fields['data'] = $data;
 $fields['to'] = $receiverRegistrationToken;
 //header with content_type api key
 $headers = array(
