7479f598d4b35fe2a0c5959e4cc08fa1d0d6dee6
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-11-22 02:26:01 +0700

    commit

--- php/get-settings.php
+++ php/get-settings.php
@@ -3,4 +3,4 @@ include 'db.php';
 /*$ip = $_SERVER["REMOTE_ADDR"];
 $userId = $c->query("SELECT * FROM sessions WHERE ip='" . $ip . "'")->fetch_assoc()["user_id"];
 $c->query("UPDATE users SET last_update=" . round(microtime(true)*1000) . " WHERE id='" . $userId . "'");*/
-echo file_get_contents("../systemdata/settings.xml");
+echo file_get_contents("http://iptvjoss.com/jossstreambe/systemdata/settings.json");
