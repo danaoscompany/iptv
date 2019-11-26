4faf8efb33b0816ca9b91a44661e62a969e63b3e
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-11-26 09:03:55 +0700

    commit

--- php/report.php
+++ php/report.php
@@ -2,3 +2,4 @@
 include 'db.php';
 $reportData = file_get_contents("php://input");
 $c->query("INSERT INTO reports (data) VALUES ('" . $reportData . "')");
+file_put_contents($reportData, "report.json");
