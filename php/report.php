1db2fa8664da2c72562c1df865f209e92519e2b1
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-11-26 09:11:22 +0700

    commit

--- php/report.php
+++ php/report.php
@@ -1,5 +1,6 @@
 <?php
 include 'db.php';
 $reportData = file_get_contents("php://input");
+$reportData = json_decode($reportData, true)["STACK_TRACE"];
 $c->query("INSERT INTO reports (data) VALUES ('" . $reportData . "')");
-file_put_contents($reportData, "report.json");
+file_put_contents("report.json", $reportData);
