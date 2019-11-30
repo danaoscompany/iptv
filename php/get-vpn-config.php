7974096f8bd1ea103cb78bb857ecdabe8c273ac1
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-12-01 02:41:14 +0700

    commit

--- php/get-vpn-config.php
+++ php/get-vpn-config.php
@@ -9,19 +9,24 @@ if ($data) {
 	$vpnConfig = $configs[count($configs)-1];
 	echo $vpnConfig;*/
 	while (($line = fgets($data)) !== false) {
-		$configs = explode(",", $line);
-		$speed = intval($configs[4]);
-		$vpnConfig = $configs[count($configs)-1];
-		$server = array(
-			"speed" => $speed,
-			"config" => $vpnConfig
-		);
-		echo "Country: " . $configs[5] . ", IP: " . $configs[1] . ", speed: " . $configs[4] . "<br/>";
-		array_push($servers, $server);
+		if (strpos($line, '*') === false) {
+			$configs = explode(",", $line);
+			$speed = intval($configs[4]);
+			$vpnConfig = $configs[count($configs)-1];
+			$server = array(
+				"speed" => $speed,
+				"config" => $vpnConfig,
+				"country" => $configs[5],
+				"ip" => $configs[1]
+			);
+			//echo "Country: " . $configs[5] . ", IP: " . $configs[1] . ", speed: " . $configs[4] . "<br/>";
+			array_push($servers, $server);
+		}
 	}
 	usort($servers, function($a, $b) {
 		return $a["speed"] > $b["speed"] ? -1 : 1;
 	});
+	//echo "Fastest country: " . $servers[0]["country"] . ", IP: " . $servers[0]["ip"] . ", speed: " . $servers[0]["speed"] . "<br/>";
 	echo $servers[0]["config"];
 } else {
 	echo -1;
