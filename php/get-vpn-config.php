18eb6ea1b33ada972d953d8ead3f9050ee0971e6
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-12-01 02:27:08 +0700

    commit

--- php/get-vpn-config.php
+++ php/get-vpn-config.php
@@ -1,10 +1,23 @@
 <?php
 $data = fopen("http://www.vpngate.net/api/iphone/", "r");
+$servers = [];
 if ($data) {
 	fgets($data);
 	fgets($data);
-	$line = fgets($data);
-	$configs = explode(",", $line);
-	$vpnConfig = $configs[count($configs)-1];
-	echo $vpnConfig;
+	while (($line = fgets($data)) !== false) {
+		$configs = explode(",", $line);
+		$speed = intval($configs[4]);
+		$vpnConfig = $configs[count($configs)-1];
+		$server = array(
+			"speed" => $speed,
+			"config" => $vpnConfig
+		);
+		array_push($servers, $server);
+	}
+	usort($servers, function($a, $b) {
+		return $a["speed"] > $b["speed"] ? -1 : 1;
+	});
+	echo $servers[0]["config"];
+} else {
+	echo -1;
 }
